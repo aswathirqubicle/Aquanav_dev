632d9e46a3b2c919b8ff52d1706006df641f034e
Sachin Vijayakumar <sachin37@bitbucket.org>
2025-12-09 13:21:55 +0530

    setup local

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,14 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+ 
+console.log("ENV TEST:", process.env.DATABASE_URL);
+ 
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../shared/schema';
-
+ 
 const connectionString = process.env.DATABASE_URL || process.env.REPLIT_DB_URL!;
-
+ 
 // Create the connection
 export const sql = postgres(connectionString);
 export const db = drizzle(sql, { schema });
--- server/index.ts
+++ server/index.ts
@@ -63,12 +63,19 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
+  //const port = 5000;
+  // server.listen({
+  //   port,
+  //   host: "0.0.0.0",
+  //   reusePort: true,
+  // }, () => {
+  //   log(`serving on port ${port}`);
+  // });
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  const host = process.platform === "win32" ? "127.0.0.1" : "0.0.0.0";
+ 
+  server.listen(port, host, () => {
+    log(`serving on http://${host}:${port}`);
   });
+  
 })();
